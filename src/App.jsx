edcc60b52ebe44a4dd4b7965fcc0586d75973861
lakshya-roonwal <erenpool7214@gmail.com>
2024-01-16 15:14:04 +0530

    Fetch The Data

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import "./App.css";
 function App() {
 	return (
     <>
-      <h1>Welcome to Joke Generator</h1>
+      <div className="main">
+        <h1>Joke Generator</h1>
+        <button className="send-btn">Generate</button>
+        <div className="joke-container">
+          <p className="joke-text"></p>
+        </div>
+      </div>
     </>
   );
 }
